2681dc06e716bb140828ef388c03444e4a3ea49c
AnaGamplay <anaoliveira360.dev@gmail.com>
2022-05-28 15:40:20 -0400

    scipt.js

--- Modulo3/to-do-list/assets/js/script.js
+++ Modulo3/to-do-list/assets/js/script.js
@@ -1,9 +1,8 @@
-let tarefaWrapper = document.getElementById("tarefa");
-let listaWrapper = document.getElementById("lista");
+let tarefa = document.getElementById("tarefa");
+let lista = document.getElementById("lista");
 let c = 0;
 
 function adicionar() {
-    listaWrapper.innerHTML += `<br><input type="checkbox" name="item${c}" class="itens"><label for="item${c}" class="txtitens">${tarefaWrapper.value}</label>`;
+    lista.innerHTML += `<br><input type="checkbox" name="item${c}" class="itens"><label for="item${c}" class="txtitens">${tarefa.value}</label>`;
     c++;
 }
-
