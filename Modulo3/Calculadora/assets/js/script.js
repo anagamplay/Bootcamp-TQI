058055c2920a1506cc13ceeac66e69d95ceca4fe
AnaGamplay <anaoliveira360.dev@gmail.com>
2022-05-29 13:07:50 -0400

    script.js

---
+++ Modulo3/Calculadora/assets/js/script.js
@@ -0,0 +1,4 @@
+function calculadora() {
+    const operacao = prompt('Escolha uma opção:\n 1 - soma (+)\n 2 - subtração (-)\n 3 - multiplicação (*)\n 4 - divisão real (/)\n 5 - divisão inteira (%)\n 6 - potenciação (**)');
+}
+calculadora()
